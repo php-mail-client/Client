63a6a10f052dbbe7d64f62d71744c2af30c5e6e0
greenyaptec <blatny.tomas@seznam.cz>
2013-03-25 11:19:44 +0100

    Created documentation

--- js/common.js
+++ js/common.js
@@ -2,6 +2,13 @@
 jQuery(function($){
 	$(document).ready(function(){
 		$('.alert').alert();
-        $('#affix').affix();
 	});
+
+    $('body').on('click', 'a', function(){
+        if($(this).attr('href').substring(0,1) == '#') {
+            $('.left-menu a').removeClass('active');
+            //alert('li a[href="'+$(this).attr('href')+'"]');
+            $('.left-menu').find('li a[href="'+$(this).attr('href')+'"]').addClass('active');
+        }
+    });
 });
