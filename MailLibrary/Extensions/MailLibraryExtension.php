c884d47eda9695bbf64b4511783bd9b00c339f92
Tomáš Blatný <blatny.tomas@seznam.cz>
2014-02-22 16:52:53 +0100

    Added Nette extension.

---
+++ MailLibrary/Extensions/MailLibraryExtension.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Tomáš Blatný
+ */
+
+namespace greeny\MailLibrary\Extensions;
+
+use Nette\DI\CompilerExtension;
+
+class MailLibraryExtension extends CompilerExtension {
+	public function loadConfiguration()
+	{
+		$config = $this->getConfig(array(
+			'username' => '',
+			'password' => '',
+			'host' => 'localhost',
+			'port' => 993,
+			'ssl' => 'true',
+		));
+
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('connection'))
+			->setClass('greeny\\MailLibrary\\Connection');
+
+		$builder->addDefinition($this->prefix('imap'))
+			->setClass('greeny\\MailLibrary\\Drivers\\ImapDriver', array(
+				$config['username'],
+				$config['password'],
+				$config['host'],
+				$config['port'],
+				$config['ssl'],
+			));
+	}
+}
+ 
