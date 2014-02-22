4dbbde972c1f4a8b7808e79229e82e9cdaf49426
Tomáš Blatný <blatny.tomas@seznam.cz>
2014-02-22 17:06:29 +0100

    Fixed loader.

--- MailLibrary/loader.php
+++ MailLibrary/loader.php
@@ -7,22 +7,22 @@ require_once "exceptions.php";
 
 spl_autoload_register(function ($type) {
 	static $paths = array(
-		'connection' => 'Connection.php',
-		'mailbox' => 'Mailbox.php',
-		'selection' => 'Selection.php',
-		'mail' => 'Mail.php',
-		'contactlist' => 'ContactList.php',
-		'attachment' => 'Attachment.php',
-		'structures\istructure' => 'Structures/IStructure.php',
-		'structures\imapstructure' => 'Structures/ImapStructure.php',
-		'drivers\idriver' => 'Drivers/IDriver.php',
-		'drivers\imapdriver' => 'Drivers/ImapDriver.php',
-		'extensions\maillibraryextension' => 'Extensions/MailLibraryExtension.php',
+		'greeny\maillibrary\connection' => 'Connection.php',
+		'greeny\maillibrary\mailbox' => 'Mailbox.php',
+		'greeny\maillibrary\selection' => 'Selection.php',
+		'greeny\maillibrary\mail' => 'Mail.php',
+		'greeny\maillibrary\contactlist' => 'ContactList.php',
+		'greeny\maillibrary\attachment' => 'Attachment.php',
+		'greeny\maillibrary\structures\istructure' => 'Structures/IStructure.php',
+		'greeny\maillibrary\structures\imapstructure' => 'Structures/ImapStructure.php',
+		'greeny\maillibrary\drivers\idriver' => 'Drivers/IDriver.php',
+		'greeny\maillibrary\drivers\imapdriver' => 'Drivers/ImapDriver.php',
+		'greeny\maillibrary\extensions\maillibraryextension' => 'Extensions/MailLibraryExtension.php',
 	);
 
 	$type = ltrim(strtolower($type), '\\'); // PHP namespace bug #49143
 
-	if (isset($paths['greeny\\maillibrary\\'.$type])) {
+	if (isset($paths[$type])) {
 		require_once __DIR__ . '/' . $paths[$type];
 	}
 });
