6a4227d0caa1e11514d0120fa418369e63b733f2
Jan Kuchař <honza.kuchar@grifart.cz>
2015-12-29 01:24:28 +0100

    loader: Fixed to be able to load Contact class (fixed indention back to tabs)

--- MailLibrary/loader.php
+++ MailLibrary/loader.php
@@ -6,24 +6,24 @@
 require_once "exceptions.php";
 
 spl_autoload_register(function ($type) {
-        static $paths = array(
-            'greeny\maillibrary\connection' => 'Connection.php',
-            'greeny\maillibrary\mailbox' => 'Mailbox.php',
-            'greeny\maillibrary\selection' => 'Selection.php',
-            'greeny\maillibrary\mail' => 'Mail.php',
-            'greeny\maillibrary\contactlist' => 'ContactList.php',
-            'greeny\maillibrary\contact' => 'Contact.php',
-            'greeny\maillibrary\attachment' => 'Attachment.php',
-            'greeny\maillibrary\structures\istructure' => 'Structures/IStructure.php',
-            'greeny\maillibrary\structures\imapstructure' => 'Structures/ImapStructure.php',
-            'greeny\maillibrary\drivers\idriver' => 'Drivers/IDriver.php',
-            'greeny\maillibrary\drivers\imapdriver' => 'Drivers/ImapDriver.php',
-            'greeny\maillibrary\extensions\maillibraryextension' => 'Extensions/MailLibraryExtension.php',
-        );
+	static $paths = array(
+		'greeny\maillibrary\connection' => 'Connection.php',
+		'greeny\maillibrary\mailbox' => 'Mailbox.php',
+		'greeny\maillibrary\selection' => 'Selection.php',
+		'greeny\maillibrary\mail' => 'Mail.php',
+		'greeny\maillibrary\contactlist' => 'ContactList.php',
+		'greeny\maillibrary\contact' => 'Contact.php',
+		'greeny\maillibrary\attachment' => 'Attachment.php',
+		'greeny\maillibrary\structures\istructure' => 'Structures/IStructure.php',
+		'greeny\maillibrary\structures\imapstructure' => 'Structures/ImapStructure.php',
+		'greeny\maillibrary\drivers\idriver' => 'Drivers/IDriver.php',
+		'greeny\maillibrary\drivers\imapdriver' => 'Drivers/ImapDriver.php',
+		'greeny\maillibrary\extensions\maillibraryextension' => 'Extensions/MailLibraryExtension.php',
+	);
 
-        $type = ltrim(strtolower($type), '\\'); // PHP namespace bug #49143
+	$type = ltrim(strtolower($type), '\\'); // PHP namespace bug #49143
 
-        if (isset($paths[$type])) {
-                require_once __DIR__ . '/' . $paths[$type];
-        }
+	if (isset($paths[$type])) {
+		require_once __DIR__ . '/' . $paths[$type];
+	}
 });
