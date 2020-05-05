0889fc0690c3070d3ed6e0e7c646d50bf294db34
Arthur Aivazov <arthur.aivazoff@gmail.com>
2020-05-06 01:24:37 +0300

    Driver getOverview

--- MailLibrary/Drivers/IDriver.php
+++ MailLibrary/Drivers/IDriver.php
@@ -11,133 +11,139 @@ use greeny\MailLibrary\Mailbox;
 use greeny\MailLibrary\Structures\IStructure;
 
 interface IDriver {
-	/**
-	 * Connects to server
-	 * @throws DriverException
-	 */
-	function connect();
+    /**
+     * Connects to server
+     * @throws DriverException
+     */
+    function connect();
 
-	/**
-	 * Flushes changes to server
-	 * @throws DriverException
-	 */
-	function flush();
+    /**
+     * Flushes changes to server
+     * @throws DriverException
+     */
+    function flush();
 
-	/**
-	 * Gets all mailboxes
-	 * @return array of string
-	 * @throws DriverException
-	 */
-	function getMailboxes();
+    /**
+     * Gets all mailboxes
+     * @return array of string
+     * @throws DriverException
+     */
+    function getMailboxes();
 
-	/**
-	 * Creates new mailbox
-	 * @param string $name
-	 * @throws DriverException
-	 */
-	function createMailbox($name);
+    /**
+     * Creates new mailbox
+     * @param string $name
+     * @throws DriverException
+     */
+    function createMailbox($name);
 
-	/**
-	 * Renames mailbox
-	 * @param string $from
-	 * @param string $to
-	 * @throws DriverException
-	 */
-	function renameMailbox($from, $to);
+    /**
+     * Renames mailbox
+     * @param string $from
+     * @param string $to
+     * @throws DriverException
+     */
+    function renameMailbox($from, $to);
 
-	/**
-	 * Deletes mailbox
-	 * @param string $name
-	 * @throws DriverException
-	 */
-	function deleteMailbox($name);
+    /**
+     * Deletes mailbox
+     * @param string $name
+     * @throws DriverException
+     */
+    function deleteMailbox($name);
 
-	/**
-	 * Switches current mailbox
-	 * @param string $name
-	 * @throws DriverException
-	 */
-	function switchMailbox($name);
+    /**
+     * Switches current mailbox
+     * @param string $name
+     * @throws DriverException
+     */
+    function switchMailbox($name);
 
-	/**
-	 * Finds UIDs of mails by filter
-	 *
-	 * @param array  $filters
-	 * @param int    $limit
-	 * @param int    $offset
-	 * @param int    $orderBy
-	 * @param string $orderType
-	 * @return array of UIDs
-	 */
-	function getMailIds(array $filters, $limit = 0, $offset = 0, $orderBy = Mail::ORDER_DATE, $orderType = 'ASC');
+    /**
+     * Finds UIDs of mails by filter
+     *
+     * @param array  $filters
+     * @param int    $limit
+     * @param int    $offset
+     * @param int    $orderBy
+     * @param string $orderType
+     * @return array of UIDs
+     */
+    function getMailIds(array $filters, $limit = 0, $offset = 0, $orderBy = Mail::ORDER_DATE, $orderType = 'ASC');
 
-	/**
-	 * Checks if filter is applicable for this driver
-	 * @param string $key
-	 * @param mixed  $value
-	 * @throws DriverException
-	 */
-	function checkFilter($key, $value = NULL);
+    /**
+     * Checks if filter is applicable for this driver
+     * @param string $key
+     * @param mixed  $value
+     * @throws DriverException
+     */
+    function checkFilter($key, $value = NULL);
 
-	/**
-	 * Gets mail headers
-	 * @param int $mailId
-	 * @return array of name => value
-	 */
-	function getHeaders($mailId);
+    /**
+     * Gets mail headers
+     * @param int $mailId
+     * @return array of name => value
+     */
+    function getHeaders($mailId);
 
-	/**
-	 * Creates structure for mail
-	 * @param int     $mailId
-	 * @param Mailbox $mailbox
-	 * @return IStructure
-	 */
-	function getStructure($mailId, Mailbox $mailbox);
+    /**
+     * Creates structure for mail
+     * @param int     $mailId
+     * @param Mailbox $mailbox
+     * @return IStructure
+     */
+    function getStructure($mailId, Mailbox $mailbox);
 
-	/**
-	 * Gets part of body
-	 * @param int   $mailId
-	 * @param array $data
-	 * @return string
-	 */
-	function getBody($mailId, array $data);
+    /**
+     * Gets part of body
+     * @param int   $mailId
+     * @param array $data
+     * @return string
+     */
+    function getBody($mailId, array $data);
 
-	/**
-	 * Gets flags for mail
-	 * @param int $mailId
-	 * @return array
-	 */
-	function getFlags($mailId);
+    /**
+     * Gets flags for mail
+     * @param int $mailId
+     * @return array
+     */
+    function getFlags($mailId);
 
-	/**
-	 * Sets one flag for mail
-	 * @param int    $mailId
-	 * @param string $flag
-	 * @param bool   $value
-	 * @throws DriverException
-	 */
-	function setFlag($mailId, $flag, $value);
+    /**
+     * Sets one flag for mail
+     * @param int    $mailId
+     * @param string $flag
+     * @param bool   $value
+     * @throws DriverException
+     */
+    function setFlag($mailId, $flag, $value);
 
-	/**
-	 * Copies mail to another mailbox
-	 * @param int    $mailId
-	 * @param string $toMailbox
-	 * @throws DriverException
-	 */
-	function copyMail($mailId, $toMailbox);
+    /**
+     * Copies mail to another mailbox
+     * @param int    $mailId
+     * @param string $toMailbox
+     * @throws DriverException
+     */
+    function copyMail($mailId, $toMailbox);
 
-	/**
-	 * Moves mail to another mailbox
-	 * @param int    $mailId
-	 * @param string $toMailbox
-	 * @throws DriverException
-	 */
-	function moveMail($mailId, $toMailbox);
+    /**
+     * Moves mail to another mailbox
+     * @param int    $mailId
+     * @param string $toMailbox
+     * @throws DriverException
+     */
+    function moveMail($mailId, $toMailbox);
 
-	/**
-	 * Deletes mail
-	 * @param int $mailId
-	 * @throws DriverException
-	 */
-	function deleteMail($mailId);
-} 
+    /**
+     * Deletes mail
+     * @param int $mailId
+     * @throws DriverException
+     */
+    function deleteMail($mailId);
+
+    /**
+     * @param $mailId
+     * @return array
+     */
+    function getOverview($mailId);
+}
--- MailLibrary/Mail.php
+++ MailLibrary/Mail.php
@@ -6,256 +6,271 @@
 namespace greeny\MailLibrary;
 
 class Mail {
-	const ANSWERED = 'ANSWERED';
-	const BCC = 'BCC';
-	const BEFORE = 'BEFORE';
-	const BODY = 'BODY';
-	const CC = 'CC';
-	const DELETED = 'DELETED';
-	const FLAGGED = 'FLAGGED';
-	const FROM = 'FROM';
-	const KEYWORD = 'KEYWORD';
-	const NEW_MESSAGES = 'NEW';
-	const NOT_KEYWORD = 'UNKEYWORD';
-	const OLD_MESSAGES = 'OLD';
-	const ON = 'ON';
-	const RECENT = 'RECENT';
-	const SEEN = 'SEEN';
-	const SINCE = 'SINCE';
-	const SUBJECT = 'SUBJECT';
-	const TEXT = 'TEXT';
-	const TO = 'TO';
+    const ANSWERED = 'ANSWERED';
+    const BCC = 'BCC';
+    const BEFORE = 'BEFORE';
+    const BODY = 'BODY';
+    const CC = 'CC';
+    const DELETED = 'DELETED';
+    const FLAGGED = 'FLAGGED';
+    const FROM = 'FROM';
+    const KEYWORD = 'KEYWORD';
+    const NEW_MESSAGES = 'NEW';
+    const NOT_KEYWORD = 'UNKEYWORD';
+    const OLD_MESSAGES = 'OLD';
+    const ON = 'ON';
+    const RECENT = 'RECENT';
+    const SEEN = 'SEEN';
+    const SINCE = 'SINCE';
+    const SUBJECT = 'SUBJECT';
+    const TEXT = 'TEXT';
+    const TO = 'TO';
 
-	const FLAG_ANSWERED = "\\ANSWERED";
-	const FLAG_DELETED = "\\DELETED";
-	const FLAG_DRAFT = "\\DRAFT";
-	const FLAG_FLAGGED = "\\FLAGGED";
-	const FLAG_SEEN = "\\SEEN";
+    const FLAG_ANSWERED = "\\ANSWERED";
+    const FLAG_DELETED = "\\DELETED";
+    const FLAG_DRAFT = "\\DRAFT";
+    const FLAG_FLAGGED = "\\FLAGGED";
+    const FLAG_SEEN = "\\SEEN";
 
-	const ORDER_DATE = SORTARRIVAL;
-	const ORDER_FROM = SORTFROM;
-	const ORDER_SUBJECT = SORTSUBJECT;
-	const ORDER_TO = SORTTO;
-	const ORDER_CC = SORTCC;
-	const ORDER_SIZE = SORTSIZE;
+    const ORDER_DATE = SORTARRIVAL;
+    const ORDER_FROM = SORTFROM;
+    const ORDER_SUBJECT = SORTSUBJECT;
+    const ORDER_TO = SORTTO;
+    const ORDER_CC = SORTCC;
+    const ORDER_SIZE = SORTSIZE;
 
-	/** @var \greeny\MailLibrary\Connection */
-	protected $connection;
+    /** @var \greeny\MailLibrary\Connection */
+    protected $connection;
 
-	/** @var \greeny\MailLibrary\Mailbox */
-	protected $mailbox;
+    /** @var \greeny\MailLibrary\Mailbox */
+    protected $mailbox;
 
-	/** @var int */
-	protected $id;
+    /** @var int */
+    protected $id;
 
-	/** @var array */
-	protected $headers = NULL;
+    /** @var array */
+    protected $headers = NULL;
 
-	/** @var \greeny\MailLibrary\Structures\IStructure */
-	protected $structure = NULL;
+    /** @var \greeny\MailLibrary\Structures\IStructure */
+    protected $structure = NULL;
 
-	/** @var array */
-	protected $flags = NULL;
+    /** @var array */
+    protected $flags = NULL;
 
-	/**
-	 * @param Connection $connection
-	 * @param Mailbox    $mailbox
-	 * @param int        $id
-	 */
-	public function __construct(Connection $connection, Mailbox $mailbox, $id)
-	{
-		$this->connection = $connection;
-		$this->mailbox = $mailbox;
-		$this->id = $id;
-	}
+    /**
+     * @var array
+     */
+    private $overview;
 
-	/**
-	 * Header checker
-	 *
-	 * @param $name
-	 * @return bool
-	 */
-	public function __isset($name)
-	{
-		$this->headers !== NULL || $this->initializeHeaders();
-		return isset($this->headers[$this->formatHeaderName($name)]);
-	}
+    /**
+     * @param Connection $connection
+     * @param Mailbox    $mailbox
+     * @param int        $id
+     */
+    public function __construct(Connection $connection, Mailbox $mailbox, $id)
+    {
+        $this->connection = $connection;
+        $this->mailbox = $mailbox;
+        $this->id = $id;
 
-	/**
-	 * Header getter
-	 *
-	 * @param string $name
-	 * @return mixed
-	 */
-	public function __get($name)
-	{
-		return $this->getHeader($name);
-	}
+        $this->overview = $this->connection->getDriver()->getOverview($id);
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @return \DateTimeImmutable|false
+     */
+    public function getDate()
+    {
+        return (new \DateTimeImmutable())->setTimestamp($this->overview->udate);
+    }
 
-	/**
-	 * @return Mailbox
-	 */
-	public function getMailbox()
-	{
-		return $this->mailbox;
-	}
+    /**
+     * Header checker
+     *
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        $this->headers !== NULL || $this->initializeHeaders();
+        return isset($this->headers[$this->formatHeaderName($name)]);
+    }
 
-	/**
-	 * @return string[]
-	 */
-	public function getHeaders()
-	{
-		$this->headers !== NULL || $this->initializeHeaders();
-		return $this->headers;
-	}
+    /**
+     * Header getter
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->getHeader($name);
+    }
 
-	/**
-	 * @param string $name
-	 * @return string
-	 */
-	public function getHeader($name)
-	{
-		$this->headers !== NULL || $this->initializeHeaders();
-		return $this->headers[$this->formatHeaderName($name)];
-	}
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return Contact|null
-	 */
-	public function getSender() {
-		$from = $this->getHeader('from');
-		if($from) {
-			$contacts = $from->getContactsObjects();
-			return (count($contacts) ? $contacts[0] : NULL);
-		} else {
-			return NULL;
-		}
-	}
+    /**
+     * @return Mailbox
+     */
+    public function getMailbox()
+    {
+        return $this->mailbox;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getBody()
-	{
-		$this->structure !== NULL || $this->initializeStructure();
-		return $this->structure->getBody();
-	}
+    /**
+     * @return string[]
+     */
+    public function getHeaders()
+    {
+        $this->headers !== NULL || $this->initializeHeaders();
+        return $this->headers;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getHtmlBody()
-	{
-		$this->structure !== NULL || $this->initializeStructure();
-		return $this->structure->getHtmlBody();
-	}
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function getHeader($name)
+    {
+        $this->headers !== NULL || $this->initializeHeaders();
+        return $this->headers[$this->formatHeaderName($name)];
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getTextBody()
-	{
-		$this->structure !== NULL || $this->initializeStructure();
-		return $this->structure->getTextBody();
-	}
+    /**
+     * @return Contact|null
+     */
+    public function getSender() {
+        $from = $this->getHeader('from');
+        if($from) {
+            $contacts = $from->getContactsObjects();
+            return (count($contacts) ? $contacts[0] : NULL);
+        } else {
+            return NULL;
+        }
+    }
 
-	/**
-	 * @return Attachment[]
-	 */
-	public function getAttachments()
-	{
-		$this->structure !== NULL || $this->initializeStructure();
-		return $this->structure->getAttachments();
-	}
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        $this->structure !== NULL || $this->initializeStructure();
+        return $this->structure->getBody();
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getFlags()
-	{
-		$this->flags !== NULL || $this->initializeFlags();
-		return $this->flags;
-	}
+    /**
+     * @return string
+     */
+    public function getHtmlBody()
+    {
+        $this->structure !== NULL || $this->initializeStructure();
+        return $this->structure->getHtmlBody();
+    }
 
-	public function setFlags(array $flags, $autoFlush = FALSE)
-	{
-		$this->connection->getDriver()->switchMailbox($this->mailbox->getName());
-		foreach(array(
-			Mail::FLAG_ANSWERED,
-			Mail::FLAG_DELETED,
-			Mail::FLAG_DELETED,
-			Mail::FLAG_FLAGGED,
-			Mail::FLAG_SEEN,
-		) as $flag) {
-			if(isset($flags[$flag])) {
-				$this->connection->getDriver()->setFlag($this->id, $flag, $flags[$flag]);
-			}
-		}
-		if($autoFlush) {
-			$this->connection->getDriver()->flush();
-		}
-	}
+    /**
+     * @return string
+     */
+    public function getTextBody()
+    {
+        $this->structure !== NULL || $this->initializeStructure();
+        return $this->structure->getTextBody();
+    }
 
-	public function move($toMailbox)
-	{
-		$this->connection->getDriver()->switchMailbox($this->mailbox->getName());
-		$this->connection->getDriver()->moveMail($this->id, $toMailbox);
-	}
+    /**
+     * @return Attachment[]
+     */
+    public function getAttachments()
+    {
+        $this->structure !== NULL || $this->initializeStructure();
+        return $this->structure->getAttachments();
+    }
 
-	public function copy($toMailbox)
-	{
-		$this->connection->getDriver()->switchMailbox($this->mailbox->getName());
-		$this->connection->getDriver()->copyMail($this->id, $toMailbox);
-	}
+    /**
+     * @return array
+     */
+    public function getFlags()
+    {
+        $this->flags !== NULL || $this->initializeFlags();
+        return $this->flags;
+    }
 
-	public function delete()
-	{
-		$this->connection->getDriver()->switchMailbox($this->mailbox->getName());
-		$this->connection->getDriver()->deleteMail($this->id);
-	}
+    public function setFlags(array $flags, $autoFlush = FALSE)
+    {
+        $this->connection->getDriver()->switchMailbox($this->mailbox->getName());
+        foreach(array(
+                    Mail::FLAG_ANSWERED,
+                    Mail::FLAG_DELETED,
+                    Mail::FLAG_DELETED,
+                    Mail::FLAG_FLAGGED,
+                    Mail::FLAG_SEEN,
+                ) as $flag) {
+            if(isset($flags[$flag])) {
+                $this->connection->getDriver()->setFlag($this->id, $flag, $flags[$flag]);
+            }
+        }
+        if($autoFlush) {
+            $this->connection->getDriver()->flush();
+        }
+    }
 
-	/**
-	 * Initializes headers
-	 */
-	protected function initializeHeaders()
-	{
-		$this->headers = array();
-		$this->connection->getDriver()->switchMailbox($this->mailbox->getName());
-		foreach($this->connection->getDriver()->getHeaders($this->id) as $key => $value) {
-			$this->headers[$this->formatHeaderName($key)] = $value;
-		}
-	}
+    public function move($toMailbox)
+    {
+        $this->connection->getDriver()->switchMailbox($this->mailbox->getName());
+        $this->connection->getDriver()->moveMail($this->id, $toMailbox);
+    }
 
-	protected function initializeStructure()
-	{
-		$this->connection->getDriver()->switchMailbox($this->mailbox->getName());
-		$this->structure = $this->connection->getDriver()->getStructure($this->id, $this->mailbox);
-	}
+    public function copy($toMailbox)
+    {
+        $this->connection->getDriver()->switchMailbox($this->mailbox->getName());
+        $this->connection->getDriver()->copyMail($this->id, $toMailbox);
+    }
 
-	protected function initializeFlags()
-	{
-		$this->connection->getDriver()->switchMailbox($this->mailbox->getName());
-		$this->flags = $this->connection->getDriver()->getFlags($this->id);
-	}
+    public function delete()
+    {
+        $this->connection->getDriver()->switchMailbox($this->mailbox->getName());
+        $this->connection->getDriver()->deleteMail($this->id);
+    }
 
-	/**
-	 * Formats header name (X-Received-From => xReceivedFrom)
-	 *
-	 * @param string $name
-	 * @return string
-	 */
-	protected function formatHeaderName($name)
-	{
-		return lcfirst(preg_replace_callback("~-.~", function($matches){
-			return ucfirst(substr($matches[0], 1));
-		}, $name));
-	}
+    /**
+     * Initializes headers
+     */
+    protected function initializeHeaders()
+    {
+        $this->headers = array();
+        $this->connection->getDriver()->switchMailbox($this->mailbox->getName());
+        foreach($this->connection->getDriver()->getHeaders($this->id) as $key => $value) {
+            $this->headers[$this->formatHeaderName($key)] = $value;
+        }
+    }
+
+    protected function initializeStructure()
+    {
+        $this->connection->getDriver()->switchMailbox($this->mailbox->getName());
+        $this->structure = $this->connection->getDriver()->getStructure($this->id, $this->mailbox);
+    }
+
+    protected function initializeFlags()
+    {
+        $this->connection->getDriver()->switchMailbox($this->mailbox->getName());
+        $this->flags = $this->connection->getDriver()->getFlags($this->id);
+    }
+
+    /**
+     * Formats header name (X-Received-From => xReceivedFrom)
+     *
+     * @param string $name
+     * @return string
+     */
+    protected function formatHeaderName($name)
+    {
+        return lcfirst(preg_replace_callback("~-.~", function($matches){
+            return ucfirst(substr($matches[0], 1));
+        }, $name));
+    }
 }
